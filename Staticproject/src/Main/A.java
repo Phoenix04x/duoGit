e7b75f77705333de9da579246ec42ff9d1ea83e8
chrisd <chrisd@localhost>
2022-06-13 13:49:05 +0200

    First

---
+++ Staticproject/src/Main/A.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class A {
+	static int a = 12;
+	int b = 42;
+	
+	static void methode1() {
+		System.out.println("methode1 = "+ a);
+	}
+	
+	void methode2() {
+		System.out.println("methode 2 = "+ a + " " + b);
+}
+
+	
+
+}
---
+++ Staticproject/src/Main/Exmethodestatic.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Exmethodestatic {
+	
+	public static void main(String[] args) {
+		
+		A.methode1();
+		//A.methode2();
+		
+		A v = new A();
+		
+		v.methode1();
+		v.methode2();
+		
+		
+	}
+
+}
